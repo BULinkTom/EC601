a15f9a89953621ada81fbd56c92269f7cd205a8d
Link <huangjl@bu.edu>
2023-10-28 18:15:03 -0400

    Initial commit

--- GoogleMap/AppDelegate.swift
+++ GoogleMap/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        GMSPlacesClient.provideAPIKey("AIzaSyDMr_gW8pE8qxtR74souX0D03c1WR2hlYw")
+        
         return true
     }
 
